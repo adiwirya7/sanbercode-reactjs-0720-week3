ff4957ce2ed04e87f0b3f1c811cfc0c308c5656d
adi wirya <50317150+adiwirya7@users.noreply.github.com>
2020-08-03 23:19:22 +0700

    Initial commit

---
+++ src/tugas11/Table.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+let dataHargaBuah = [
+  {nama: "Semangka", harga: 10000, berat: 1000},
+  {nama: "Anggur", harga: 40000, berat: 500},
+  {nama: "Strawberry", harga: 30000, berat: 400},
+  {nama: "Jeruk", harga: 30000, berat: 1000},
+  {nama: "Mangga", harga: 30000, berat: 500}
+]
+class Row extends React.Component{
+    render(){
+        return (
+            dataHargaBuah.map(el=> {
+                return (
+                    <tr>
+                    <td>{el.nama}</td>
+                    <td>{el.harga}</td>
+                    <td>{el.berat/1000} kg</td>
+                    </tr>   
+                )
+            })
+        )
+    }
+}
+
+class Table extends React.Component {
+    render() {
+        return (
+            <div style={{width: "80%", margin:"auto"}}>
+                    <h1 style={{textAlign:"center"}}>Tabel Harga Buah</h1>
+                    <table style={{width:"100%"}}>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Harga</th>
+                        <th>Berat</th>
+                    </tr>
+                    <Row />
+                    </table>
+                </div>
+        )
+    }
+}
+
+export default Table
