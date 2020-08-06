266b036fd05c73bb046c905c4ce40f20ad0bb904
adi wirya <50317150+adiwirya7@users.noreply.github.com>
2020-08-07 02:29:48 +0700

    Tugas 14

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './App.css';
 // import Table from './tugas11/Table'
 // import Timer from './tugas12/Timer'
- import Form from './tugas13/Form'
+//  import Form from './tugas13/Form'
+ import Hooks from './tugas14/Hooks'
 
 function App() {
   return (
     <div >
       {/* <Table/> */}
       {/* <Timer/> */}
-      <Form/>
+      {/* <Form/> */}
+      <Hooks/>
     </div>
   );
 }
